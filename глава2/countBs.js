e35c27422008c505a12a9a268ad21e9f64645a14
IgorKonovalov <konovalov.avp@gmail.com>
2016-11-01 21:09:02 +0300

    улучшение uxui

--- глава2/countBs.js
+++ глава2/countBs.js
@@ -1,5 +1,6 @@
 var str = "This is a test string with some Beans in it and some more Beans";
 var count = 0;
+var char = 'T';
 function countChar(str, char) {
   for (var i = 0; i < str.length; i++) {
     if (str.charAt(i) == char) count++;
@@ -7,6 +8,6 @@ function countChar(str, char) {
   return count;
 }
 
-countChar(str, 't');
+countChar(str, char);
 
-console.log(count);
+console.log("Вы искали символ: " + char + " он встречается в строке: " + count + " раз");
