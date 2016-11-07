e189da55bd6906480dded332f189da48e276076a
IgorKonovalov <konovalov.avp@gmail.com>
2016-11-07 22:35:34 +0300

    третья задача 3й главы

---
+++ глава3/lists.js
@@ -0,0 +1,53 @@
+// var list = {
+//   value: 1,
+//   rest: {
+//     value: 2,
+//     rest: {
+//       value: 3,
+//       rest: null
+//     }
+//   }
+// };
+function prepend(value, list) {
+  list = {value: value, rest: list}
+  return list;
+}
+var list_temp = { value: 10, rest: { value: 20, rest: null } };
+
+function arrayToList(array) {
+  var list = null;
+  for (var i = array.length - 1; i >= 0; i--) {
+    list = prepend(array[i],list);
+  }
+  return list;
+}
+
+function listToArray(list) {
+  var array = [];
+  while (list.rest != null) {
+    array.push(list.value);
+    list = list.rest;
+  }
+  array.push(list.value);
+  return array;
+}
+
+
+function nth(list, position) {
+  var element;
+  var count = 0;
+  if (position == 0) element = list.value;
+  for (var i = 0; i < position; i++) {
+    if (list.rest != null) {
+      list = list.rest;
+      element = list.value;
+    }
+    else element = undefined;
+  }
+  return element;
+}
+
+
+
+console.log(listToArray(arrayToList([10,20,30])));
+console.log(nth(arrayToList([10,20,30]),1));
