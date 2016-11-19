529a4bc7c2a154570eb000acb49eff29cabde63b
IgorKonovalov <konovalov.avp@gmail.com>
2016-11-19 20:28:25 +0300

    добавки к примерам пятой главы

--- examples/глава5/table.js
+++ examples/глава5/table.js
@@ -2,15 +2,20 @@ var mountains = require('./mountains.js');
 
 // console.log(mountains);
 
-function rowHeights(rows) {
-  return rows.map(function (row) {
-    return row.reduce(function (max, cell) {
+// minHeight возвращает число, показывающее минимельную высоту, которую требует ячейка (в строчках)
+// minWidth возвращает число, показывающее минимальную ширину, которую треует ячейка (в символах)
+// draw(width, height) возвращает массив длины height, содержащий наборы строк, каждая из которых
+// шириной width символов. Это содержимое ячейки.
+
+function rowHeights(rows) { // вычисление массива минимальной высоты строк
+  return rows.map(function (row) { // возвращает массив, к каждому элементу которого применена функция
+    return row.reduce(function (max, cell) { // получающая максимальную высоту из всех ячеек
       return Math.max(max, cell.minHeight());
     }, 0);
   });
 }
 
-function colWidths(rows) {
+function colWidths(rows) { // вычисление массива минимальной ширины ячеек
   return rows[0].map(function (_, i) {
     return rows.reduce(function (max, row) {
       return Math.max(max, row[i].minWidth());
@@ -23,21 +28,23 @@ function drawTable(rows) {
   var widths = colWidths(rows);
 
   function drawLine(blocks, lineNo) {
-    return blocks.map(function (block) {
+    return blocks.map(function(block) {
       return block[lineNo];
     }).join(" ");
   }
 
   function drawRow(row, rowNum) {
-    var blocks = row.map(function (cell, colNum) {
-      return cell.draw(widths[colNum], heights[colNum]);
+    var blocks = row.map(function(cell, colNum) {
+      return cell.draw(widths[colNum], heights[rowNum]);
     });
-    return blocks[0].map(function (_,lineNo) {
+    return blocks[0].map(function(_, lineNo) {
       return drawLine(blocks, lineNo);
     }).join("\n");
   }
+
   return rows.map(drawRow).join("\n");
 }
+
 
 function repeat(string, times) {
   var result = "";
@@ -46,20 +53,17 @@ function repeat(string, times) {
   return result;
 }
 
-function TextCell(text) {
+function TextCell(text) { // обычная ячейка
   this.text = text.split("\n");
 }
-
-TextCell.prototype.minWidth = function() {
-  return this.text.reduce(function (width, line) {
+TextCell.prototype.minWidth = function() { // метод, возвращающий минимально возможную ширину ячейки
+  return this.text.reduce(function(width, line) {
     return Math.max(width, line.length);
   }, 0);
 };
-
 TextCell.prototype.minHeight = function() {
   return this.text.length;
 };
-
 TextCell.prototype.draw = function(width, height) {
   var result = [];
   for (var i = 0; i < height; i++) {
@@ -69,9 +73,53 @@ TextCell.prototype.draw = function(width, height) {
   return result;
 };
 
-// нарисуем шахматную доску
+function UnderlinedCell(inner) { // ячейка с подчеркиванием
+  this.inner = inner;
+}
+UnderlinedCell.prototype.minWidth = function() {
+  return this.inner.minWidth();
+};
+UnderlinedCell.prototype.minHeight = function() {
+  return this.inner.minHeight() + 1;
+};
+UnderlinedCell.prototype.draw = function(width, height) {
+  return this.inner.draw(width, height - 1)
+    .concat([repeat("-", width)]);
+};
 
-var rows = [];
+function RTextCell(text) { // ячейка со сдвигом к правому краю, наследуется от TextCell
+  TextCell.call(this, text);
+}
+RTextCell.prototype = Object.create(TextCell.prototype);
+RTextCell.prototype.draw = function (width, height) {
+  var result = [];
+  for (var i = 0; i < height; i++) {
+    var line = this.text[i] || "";
+    result.push(repeat(" ", width - line.length) + line);
+  }
+  return result;
+};
+
+function dataTable(data) {
+  var keys = Object.keys(data[0]);
+  var headers = keys.map(function (name) {
+    return new UnderlinedCell(new TextCell(name));
+  });
+  var body = data.map(function (row) {
+    return keys.map(function (name) {
+      var value = row[name];
+      if (typeof value == "number")
+        return new RTextCell(String(value));
+      else
+        return new TextCell(String(value));
+    });
+  });
+  return [headers].concat(body);
+}
+
+console.log(drawTable(dataTable(mountains)));
+
+var rows = []; // для примера
 for (var i = 0; i < 5; i++) {
   var row = [];
   for (var j = 0; j < 5; j++) {
@@ -83,36 +131,3 @@ for (var i = 0; i < 5; i++) {
   rows.push(row);
 }
 // console.log(drawTable(rows));
-
-// ячейка для выделения верхней строки таблицы
-
-function UnderlinedCell(inner) {
-  this.inner = inner;
-};
-
-UnderlinedCell.prototype.minWidth = function() {
-  return this.inner.minWidth;
-};
-
-UnderlinedCell.prototype.minHeight = function() {
-  return this.inner.minHeight() + 1;
-};
-
-UnderlinedCell.prototype.draw = function(width, height) {
-  return this.inner.draw(width. heigth - 1).concat([repeat("-", width)]);
-};
-
-function dataTable(data) {
-  var keys = Object.keys(data[0]);
-  var headers = keys.map(function (name) {
-    return new UnderlinedCell(new TextCell(name));
-  });
-  var body = data.map(function (row) {
-    return keys.map(function (name) {
-      return new TextCell(String(row[name]));
-    });
-  });
-  return [headers].concat(body);
-}
-
-console.log(drawTable(dataTable(mountains)));
