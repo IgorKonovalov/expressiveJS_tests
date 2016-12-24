221381fae620bea2f8e9fdc768afd1530387ef63
Igor Konovalov <konovalov.avp@gmail.com>
2016-12-24 21:20:12 +0300

    answer

--- chapter15-Canvas/exercises/bouncing_ball/index.js
+++ chapter15-Canvas/exercises/bouncing_ball/index.js
@@ -13,7 +13,7 @@ function frame(time) {
   requestAnimationFrame(frame);
 }
 requestAnimationFrame(frame);
-
+//
 let x = canvas.width / 2;
 let y = canvas.height - ballRadius;
 let dx = -2;
@@ -37,5 +37,28 @@ function updateAnimation(step) {
     dx = - dx;
   if ((y > canvas.height - ballRadius) || (y < 0 + ballRadius))
     dy = - dy;
-  console.log("dx: " + dx + "dy: " + dy);
 }
+
+// элегантное решение (ответ)
+
+// var x = 100, y = 300;
+// var radius = 10;
+// var speedX = 100, speedY = 60;
+//
+// function updateAnimation(step) {
+//   cx.clearRect(0, 0, 400, 400);
+//   cx.strokeStyle = "blue"; // обводка
+//   cx.lineWidth = 4;
+//   cx.strokeRect(25, 25, 350, 350);
+//
+//   x += step * speedX;
+//   y += step * speedY;
+//   if (x < 25 + radius || x > 375 - radius)
+//     speedX = -speedX;
+//   if (y < 25 + radius || y > 375 - radius)
+//     speedY = -speedY;
+//   cx.fillStyle = "red";
+//   cx.beginPath();
+//   cx.arc(x, y, radius, 0, 7);
+//   cx.fill();
+// }
