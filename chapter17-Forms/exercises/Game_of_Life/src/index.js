c36eea8479cc4958033f2d6e47135210af3882db
IgorKonovalov <konovalov.avp@gmail.com>
2017-01-02 21:35:25 +0300

    changing box state on click

--- chapter17-Forms/exercises/Game_of_Life/src/index.js
+++ chapter17-Forms/exercises/Game_of_Life/src/index.js
@@ -5,35 +5,80 @@ const cx = document.querySelector("canvas").getContext("2d");
 const scale = 40; // в будущем будет меняться
 const rowCount = Math.floor(cx.canvas.height / scale);
 const columnCount = Math.floor(cx.canvas.width / scale);
+const color = "#0095DD";
 
-// создаем массив клеток (все незаполнены)
+console.log("columns: " + columnCount + " rows: " + rowCount);
+// создаем массив клеток
 
-console.log(rowCount + " " + columnCount);
-
-function Vector(x, y) {
-  this.x = x;
-  this.y = y;
+let boxes = [];
+for (let c = 0; c < columnCount; c++) {
+  boxes[c] = [];
+  for (let r = 0; r < rowCount; r++) {
+    boxes[c][r] = {x: 0, y: 0, status: 0}; // получаем массив, с номером column, row, в котором лежит объект с кординатами 0,0 и статусом
+  }
 }
 
-Vector.prototype.plus = function(other) {
-  return new Vector(this.x + other.x, this.y + other.y);
-};
+// рандомизируем статус клеток
 
-function isPointInBox(p, column, row) { // проверка, попадаем ли мы в клетку
-  let b = boxPosition(column, row);
-  return !(p.x < b.x ||
-           p.y < b.y ||
-           p.x > b.x + scale ||
-           p.y > b.y + scale);
-};
+function boxRandomize() {
+  boxes.forEach(function(boxRow) {
+    boxRow.forEach(function(box) {
+      box.status = Math.round(Math.random());
+    });
+  });
+}
 
-function boxPosition(column, row) { // возвращает пиксельные координаты клетки по column и row
-  return {
-    x: column * scale,
-    y: row * scale
-  };
-};
+boxRandomize();
 
+
+(function updateBoxOnClick() {
+  document.getElementById("game").addEventListener("click", function(e) {
+    let p = {x: e.offsetX, y: e.offsetY}; // обьект с позицией клика
+    boxes.forEach(function(boxcolumn) {
+      boxcolumn.forEach(function(box) {
+        if (clickOnBox(p, box)) {
+          if (box.status == 0) {
+            box.status = 1;
+            updateAnimation();
+          }
+          if (box.status == 1) {
+            box.status = 0;
+            updateAnimation();
+          }
+        };
+      });
+    });
+  });
+})();
+
+function updateAnimation() {
+  draw();
+}
+
+function drawBoxes(boxes) {
+  for (let c = 0; c < columnCount; c++) {
+    for (let r = 0; r < rowCount; r++) {
+      let boxX = c * scale;
+      let boxY = r * scale;
+      boxes[c][r].x = boxX;
+      boxes[c][r].y = boxY;
+      if (boxes[c][r].status == 1) {
+        cx.beginPath();
+        cx.rect(boxX, boxY, scale, scale);
+        cx.fillStyle = color;
+        cx.fill();
+        cx.closePath();
+      }
+    }
+  }
+}
+
+function clickOnBox(p, box) {
+  return !(p.x < box.x ||
+           p.y < box.y ||
+           p.x > box.x + scale ||
+           p.y > box.y + scale);
+}
 
 
 
@@ -53,23 +98,19 @@ function drawGrid() { // рисуем решетку
 }
 
 
-
-function updateSquareOnClick() {
-  document.getElementById("game").addEventListener("click", function(e) {
-    let p = { x: e.offsetX, y: e.offsetY }; // p - position
-
-    cx.fillStyle="#FF0000";  // проверка
-    cx.fillRect(p.x, p.y, scale, scale);
-  });
-}
-
 function draw() {
-  cx.clearRect(0, 0, cx.canvas.height, cx.canvas.width);
+  cx.clearRect(0, 0, cx.canvas.width, cx.canvas.height);
+  drawBoxes(boxes);
   drawGrid();
-  updateSquareOnClick()
-  //drawLife();
-}
-
-// поехали!
+  console.log("рисую");
+};
 
 draw();
+
+let button = document.getElementById("test");
+console.log(button);
+button.addEventListener("click", function () {
+  console.log("нажал");
+  draw();
+});
+// поехали!
