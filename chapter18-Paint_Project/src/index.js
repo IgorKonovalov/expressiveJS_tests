fd09a7dca1f89715fffdeb5b7f974997d4948aca
Igor Konovalov <konovalov.avp@gmail.com>
2017-01-08 14:13:06 +0300

    fill

--- chapter18-Paint_Project/src/index.js
+++ chapter18-Paint_Project/src/index.js
@@ -264,6 +264,54 @@ tools["Pick color"] = function (event, cx) {
 
 };
 
+// 3 - заливка. Это совсем. Ответыыы
+
+// вызываем функцию для всех соседей по горизонтали / вертикали
+function forAllNeighbors(point, fn) {
+  fn({x: point.x, y: point.y + 1});
+  fn({x: point.x, y: point.y - 1});
+  fn({x: point.x + 1, y: point.y});
+  fn({x: point.x - 1, y: point.y});
+}
+
+// рассматривает 2 позиции, возвращает true если цвет одинаков
+function isSameColor(data, pos1, pos2) {
+  let offset1 = (pos1.x + pos1.y * data.width) * 4;
+  let offset2 = (pos2.x + pos2.y * data.width) * 4;
+  for (let i = 0; i < 4; i++) {
+    if (data.data[offset1 + i] != data.data[offset2 + i])
+      return false;
+  }
+  return true;
+}
+
+tools["Flood fill"] = function(event, cx) {
+  let startPos = relativePos(event, cx.canvas);
+
+  let data = cx.getImageData(0, 0, cx.canvas.width,
+                             cx.canvas.height);
+  // одномерный массив, длиной в картинку.
+  let alreadyFilled = new Array(data.width * data.height);
+
+  // список еще не обработанных координат одинакового цвета
+  let workList = [startPos];
+  while (workList.length) {
+    let pos = workList.pop();
+    console.log(pos);
+    let offset = pos.x + data.width * pos.y;
+    if (alreadyFilled[offset]) continue;
+
+    cx.fillRect(pos.x, pos.y, 1, 1);
+    alreadyFilled[offset] = true;
+
+    forAllNeighbors(pos, function(neighbor) {
+      if (neighbor.x >= 0 && neighbor.x < data.width &&
+          neighbor.y >= 0 && neighbor.y < data.height &&
+          isSameColor(data, startPos, neighbor))
+        workList.push(neighbor);
+    });
+  }
+};
 
 // запуск
 let body = document.getElementById("paint")
