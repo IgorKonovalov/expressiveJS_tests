c64cf56263ee663ae5a20cf0b5af56c4b63855ca
Igor Konovalov <konovalov.avp@gmail.com>
2017-01-09 19:30:23 +0300

    simple client-server example

---
+++ chapter19-NODEJS/examples/http_module/client.js
@@ -0,0 +1,11 @@
+let http = require("http");
+let request = http.request({
+  hostname: "eloquentjavascript.net",
+  path: "/20_node.html",
+  method: "GET",
+  headers: {Accept: "text/html"}}, function(response) {
+    console.log("Сервер ответил с кодом ", response.statusCode);
+});
+request.end();
+
+// простейший клиент. Запусти! 
