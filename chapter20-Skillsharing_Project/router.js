500bd350e8c7a94099412dcf3e27cf40741e844a
IgorKonovalov <konovalov.avp@gmail.com>
2017-01-21 15:00:06 +0300

    router code comments

--- chapter20-Skillsharing_Project/router.js
+++ chapter20-Skillsharing_Project/router.js
@@ -1,23 +1,23 @@
-var Router = module.exports = function() {
+var Router = module.exports = function() { // экспортируем конструктор роутер
   this.routes = [];
 };
 
-Router.prototype.add = function(method, url, handler) {
+Router.prototype.add = function(method, url, handler) { // роутер является всомогательным модулем, в который можно добавить различные методы - то, как будет реагировать сервер на клиента. Здесь мы добавляем методы
   this.routes.push({method: method,
                     url: url,
                     handler: handler});
 };
 
-Router.prototype.resolve = function(request, response) {
-  var path = require("url").parse(request.url).pathname;
+Router.prototype.resolve = function(request, response) { // распределяем запросы
+  var path = require("url").parse(request.url).pathname; // парсим путь запроса
 
-  return this.routes.some(function(route) {
-    var match = route.url.exec(path);
+  return this.routes.some(function(route) { // пробуем пути по очереди, и останавливаемся с true если путь найден
+    var match = route.url.exec(path); // exec выполняет дочерний процесс в буфере
     if (!match || route.method != request.method)
       return false;
 
-    var urlParts = match.slice(1).map(decodeURIComponent);
-    route.handler.apply(null, [request, response]
+    var urlParts = match.slice(1).map(decodeURIComponent); // берем первый кусок match - декодируем его для корректного пути
+    route.handler.apply(null, [request, response] // и вызываем handler с подготовленным для него путем
                                 .concat(urlParts));
     return true;
   });
